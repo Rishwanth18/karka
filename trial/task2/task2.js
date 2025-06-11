9d1a4d6036eb1b697b344d46fdad26aafb63b24d
Rishwanth18 <rishwanth.002@gmail.com>
2025-06-11 03:19:20 -0700

    task2

---
+++ trial/task2/task2.js
@@ -0,0 +1,151 @@
+// // // task 1
+// let firstName="sunil"
+// let lastName="kumar"
+// console.log("firstName",firstName,"lastName",lastName)
+
+// // task 2
+// function add(){
+//     a=20
+//     b=30
+//     c=a+b
+//     console.log(c)
+// }
+// add()
+
+// task 3
+// function mul(pi,r,r){
+//     let c=(pi*r*r)
+//     console.log(c)
+// }
+// mul(10,20,30)
+
+// task 4
+// function mul(l,b){
+//     let c=(l*b)
+//     console.log(c)
+// }
+// mul(10,5)
+
+// task 5
+// let b=10
+// let h=20
+// let c=(b*h)/2
+// console.log(c)
+
+// task 6
+// let a=10
+// let b=20
+// let c=a+b
+// console.log(c)
+
+// let a=10
+// let b=20
+// let c=a-b
+// console.log(c)
+
+// let a=10
+// let b=20
+// let c=a*b
+// console.log(c)
+
+// let a=10
+// let b=20
+// let c=a/b
+// console.log(c)
+
+// task 7
+// let a=100
+// a+=50
+// console.log(a)
+
+// let a=100
+// a-=50
+// console.log(a)
+
+// let a=100
+// a*=50
+// console.log(a)
+
+// let a=100
+// a/=50
+// console.log(a)
+
+// task 8
+// let a=100
+// a+=20
+// a-=30
+// console.log(a)
+
+// task 9
+// let a=100
+// let b=100
+// console.log(a==b)
+
+// let a=100
+// let b=100
+// console.log(a===b)
+
+// let a=100
+// let b=80
+// console.log(a!=b)
+
+// let a=100
+// let b=101
+// console.log(a>=b)
+
+// let a=100
+// let b=10
+// console.log(a<=b)
+
+// task 10
+// let a=100
+// let b=10
+// console.log(a>b&&b<a)
+
+// let a=100
+// let b=10
+// console.log(a<b||a>b)
+ 
+// let a=50
+// let b=60
+// console.log(a<b!=b>a)
+
+// task 11
+// let a=10
+// let b=20
+// let c=a
+// a=b
+// b=c
+// console.log("a=",a)
+// console.log("b=",b)
+
+// let a=10
+// let b=20
+// a=a+b
+// b=a-b
+// a=a-b
+// console.log("a",a)
+// console.log("b",b)
+
+// task 12
+// let a=10
+// let b=20
+// let c=30
+// console.log((a+b+c)/3)
+
+// task 13
+// let a=10
+// let b=20
+// let c=10
+// let d=5
+// console.log(a+b)
+// console.log((a+b)*c)
+// console.log(((a+b)*c)/d)
+
+// task 14
+let tamil=85
+let english=84
+let maths=80
+let science=87
+let social=90
+console.log((tamil+english+maths+science+social)/5)
