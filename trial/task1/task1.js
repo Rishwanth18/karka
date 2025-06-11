6475e9d5ff1aca54a1affbd4903cecab4579ea9c
Rishwanth18 <rishwanth.002@gmail.com>
2025-06-11 02:38:53 -0700

    task1

---
+++ trial/task1/task1.js
@@ -0,0 +1,84 @@
+// task 1
+// console.log("Hello,World!")
+
+// task 2
+// console.log("rishi")
+
+// task 3
+// console.log("rishi")
+// console.log(22)
+
+// task 4
+// let name="rishi"
+// let age=22
+// let weight=62.5
+// let marrial_status="no"
+// console.log("name:",name)
+// console.log("age:",age)
+// console.log("weight:",weight)
+// console.log("marrial_status:",marrial_status)
+
+// task 5
+// let name="rishi"
+// let age=22
+// console.log("name:",name,"age:",age)
+
+// task  6
+//  let name="rishi"
+// let age=22
+//  console.log("name:",name)
+// console.log("age:",age)
+
+// task 7
+// let a=10
+// let b=20
+// let c=a+b
+// console.log(c)
+
+// let a=10
+// let b=20
+// let c=a-b
+// console.log(c)
+
+// let a=10
+// let b=20
+// let c=a*b
+// console.log(c)
+
+// let a=20
+// let b=20
+// let c=a/b
+// console.log(c)
+
+// task 8
+// console.log("*")
+// console.log("**")
+// console.log("***")
+// console.log("****")
+
+// task 9
+// let name="rishi"
+// let age=23
+// let isStudent="false"
+// console.log("name",name)
+// console.log("age",age)
+// console.log("isStudent",isStudent)
+
+
+
+
+// task 10
+//  let name="rishi"
+// let age=22
+// let weight=62.5
+// let dob="18/09/2003"
+// let height=175
+// let degree="b.e"
+// let gender="male"
+// console.log("name:",name)
+// console.log("age:",age)
+// console.log("weight:",weight)
+// console.log("dob:",dob)
+// console.log("height:",height)
+// console.log("degree:",degree)
+// console.log("gender:",gender)
